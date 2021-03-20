c134717d10f926335b67392d6480dcbb9d0301d2
Hector Satizabal <hector.satizabal@areamovil.com.co>
2021-03-20 17:27:16 -0500

    Permissions were added to the storage device

--- ToDo/App.xaml.cs
+++ ToDo/App.xaml.cs
@@ -1,6 +1,8 @@
-﻿using Prism;
+﻿using Plugin.Permissions.Abstractions;
+using Prism;
 using Prism.Ioc;
 using Prism.Unity;
+using ToDo.Common;
 using ToDo.View;
 using ToDo.ViewModels;
 using Xamarin.Forms;
@@ -17,6 +19,8 @@ namespace ToDo
         {
             InitializeComponent();
 
+            InitializeAppPermissionsAsync();
+
             await NavigationService.NavigateAsync("NavigationPage/MainPage");
         }
 
@@ -24,6 +28,13 @@ namespace ToDo
         {
             containerRegistry.RegisterForNavigation<NavigationPage>();
             containerRegistry.RegisterForNavigation<MainPage, ToDoViewModel>();
+        }
+
+        private async void InitializeAppPermissionsAsync()
+        {
+            var hasPermission = await Utils.CheckPermissions(Permission.Storage);
+            if (!hasPermission)
+                return;
         }
     }
 }
