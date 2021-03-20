c1dfc65cc3ac7529da2ab6feb39a28521d20df5a
Hector Satizabal <hector.satizabal@areamovil.com.co>
2021-03-20 16:37:42 -0500

    Business logic was added to VM

---
+++ ToDo/Model/Task.cs
@@ -0,0 +1,7 @@
+﻿namespace ToDo.Model
+{
+    public class Task
+    {
+        public string Note { get; set; }
+    }
+}
--- ToDo/ViewModels/ToDoViewModel.cs
+++ ToDo/ViewModels/ToDoViewModel.cs
@@ -1,11 +1,43 @@
-﻿using Prism.Mvvm;
+﻿using System.Collections.ObjectModel;
+using Prism.Mvvm;
+using ToDo.Model;
+using Xamarin.Forms;
 
 namespace ToDo.ViewModels
 {
     public class ToDoViewModel : BindableBase
     {
+        DataStore dataStore = new DataStore();
+
         public ToDoViewModel()
         {
+            addCommand = new Command(agregarevent);
+        }
+
+        private void agregarevent()
+        {
+            dataStore.Add(note);
+        }
+
+        private string note;
+
+        public string Note
+        {
+            get { return note; }
+            set { note = value; }
+        }
+
+        public ObservableCollection<Task> Tasks
+        {
+            get { return dataStore.Tasks; }
+        }
+
+        private Command addCommand;
+
+        public Command Add
+        {
+            get { return addCommand; }
+            set { addCommand = value; }
         }
     }
 }
